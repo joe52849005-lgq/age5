b8e7e8ebf27d4378a1adac5968db80a7b0c3e0ea
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-09-23 07:18:13 +0300

    Fix ship ownership buff not working issue. (#1094) by MangoXing

--- AAEmu.Game/Core/Managers/SlaveManager.cs
+++ AAEmu.Game/Core/Managers/SlaveManager.cs
@@ -397,9 +397,15 @@ public class SlaveManager : Singleton<SlaveManager>
         // Check if the target spot is already taken
         var slave = GetSlaveByObjId(objId);
 
-        //var slave = GetActiveSlaveByObjId(objId);
         if (slave == null || slave.AttachedCharacters.ContainsKey(attachPoint))
             return;
+
+        // Check if the vehicle has the MasterOwnership buff and if the character is not the owner, block the attachment.
+        if (attachPoint == AttachPointKind.Driver && slave.Buffs.CheckBuff((uint)BuffConstants.MasterOwnership) && slave.Summoner.ObjId != character.ObjId)
+        {
+            character.SendErrorMessage(ErrorMessageType.SlaveAlreadyHasMaster); // 仅阻止驾驶座附加
+            return;
+        }
 
         character.BroadcastPacket(new SCUnitAttachedPacket(character.ObjId, attachPoint, bondKind, objId), true);
         character.AttachedPoint = attachPoint;
