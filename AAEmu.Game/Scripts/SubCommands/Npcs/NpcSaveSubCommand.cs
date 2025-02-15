eac60d6a634dde23c257e8f49bf572932c021014
NLObP <38990417+NL0bP@users.noreply.github.com>
2025-02-16 01:47:01 +0300

    [Game/Scripts] Changed the NpcSaveSubCommand class: - read data from npc_spawns*.json - Npc duplicate check added; - write the Npc data to the file npc_spawns_all_{DateTime.Now:yyyyMMdd_HHmmss}.json.add;

--- AAEmu.Game/Scripts/SubCommands/Npcs/NpcSaveSubCommand.cs
+++ AAEmu.Game/Scripts/SubCommands/Npcs/NpcSaveSubCommand.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-using AAEmu.Commons.Exceptions;
 using AAEmu.Commons.IO;
 using AAEmu.Commons.Utils;
 using AAEmu.Commons.Utils.Creatures;
 using AAEmu.Game.Core.Managers;
 using AAEmu.Game.Core.Managers.World;
 using AAEmu.Game.Models.Game.Char;
-using AAEmu.Game.Models.Game.DoodadObj;
 using AAEmu.Game.Models.Game.World;
 using AAEmu.Game.Models.Json;
 using AAEmu.Game.Utils;
@@ -27,6 +26,8 @@ namespace AAEmu.Game.Scripts.SubCommands.Npcs;
 public class NpcSaveSubCommand : SubCommandBase
 {
     private static Dictionary<uint, Creature> _creatures;
+    private bool _isSavingInProgress = false;
+    private readonly object _saveLock = new object();
 
     public NpcSaveSubCommand()
     {
@@ -63,75 +64,143 @@ public class NpcSaveSubCommand : SubCommandBase
 
     private void SaveAll(ICharacter character, IMessageOutput messageOutput)
     {
-        _creatures = Creature.GetAllCreatures();
-        var currentWorld = WorldManager.Instance.GetWorld(((Character)character).Transform.WorldId);
-        var allNpcs = WorldManager.Instance.GetAllNpcs();
-        var npcsInWorld = WorldManager.Instance.GetAllNpcsFromWorld(currentWorld.Id);
-        var npcSpawnersFromFile = LoadNpcsFromFileByWorld(currentWorld);
-        var npcSpawnersToFile = npcSpawnersFromFile.ToList();
-
-        foreach (var npc in npcsInWorld.Where(n => n.Spawner is not null))
+        // Проверка на выполнение записи
+        if (_isSavingInProgress)
         {
-            switch (npc.Spawner.Id)
-            {
-                // spawned into the game manually
-                case 0:
-                    {
-                        var pos = npc.Transform.World;
-                        var newNpcSpawn = new JsonNpcSpawns
-                        {
-                            Id = allNpcs.Last().ObjId++,
-                            UnitId = npc.TemplateId,
-                            Title = GetSpawnName(npc.TemplateId), // обновим Title
-                            Position = new JsonPosition
-                            {
-                                X = pos.Position.X,
-                                Y = pos.Position.Y,
-                                Z = pos.Position.Z,
-                                Roll = pos.Rotation.X.RadToDeg(),
-                                Pitch = pos.Rotation.Y.RadToDeg(),
-                                Yaw = pos.Rotation.Z.RadToDeg()
-                            },
-                            Scale = npc.Scale
-                        };
-
-                        npcSpawnersToFile.Add(newNpcSpawn);
-                        break;
-                    }
-
-                // removed from the game manually
-                case 0xffffffff: //(uint)-1
-                    {
-                        // Do not add to the output of a manually remote Npc
-                        var npcSpawnsToRemove = new List<JsonNpcSpawns>();
-
-                        foreach (var npcSpawnerToFile in npcSpawnersToFile.Where(n => n.UnitId == npc.TemplateId))
-                        {
-                            // If the position changed don't mark to be removed
-                            if (!npc.Transform.World.Position.Equals(npcSpawnerToFile.Position.AsVector3()))
-                            {
-                                continue;
-                            }
-
-                            npcSpawnsToRemove.Add(npcSpawnerToFile);
-                            break;
-                        }
-
-                        foreach (var npcSpawn in npcSpawnsToRemove)
-                        {
-                            npcSpawnersToFile.Remove(npcSpawn);
-                        }
-
-                        break;
-                    }
-            }
+            SendMessage(messageOutput, "Save operation is already in progress.");
+            return;
         }
 
-        var jsonPathOut = Path.Combine(FileManager.AppPath, "Data", "Worlds", currentWorld.Name, "npc_spawns_new.json");
-        var json = JsonConvert.SerializeObject(npcSpawnersToFile.ToArray(), Formatting.Indented, new JsonModelsConverter());
-        File.WriteAllText(jsonPathOut, json);
-        SendMessage(messageOutput, "All npcs have been saved!");
-        Logger.Info("All npcs have been saved!");
+        lock (_saveLock)
+        {
+            if (_isSavingInProgress)
+            {
+                SendMessage(messageOutput, "Save operation is already in progress.");
+                return;
+            }
+
+            _isSavingInProgress = true;
+        }
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        try
+        {
+            _creatures = Creature.GetAllCreatures();
+            var currentWorld = WorldManager.Instance.GetWorld(((Character)character).Transform.WorldId);
+            var npcsInWorld = WorldManager.Instance.GetAllNpcsFromWorld(currentWorld.Id);
+            var npcSpawnersFromFile = LoadNpcsFromFileByWorld(currentWorld);
+            var npcSpawnersToFile = npcSpawnersFromFile.ToList();
+
+            var addNpcs = npcsInWorld.Where(n => n.Spawner?.Id == 0).ToList();
+            var removeNpcs = npcsInWorld.Where(n => n.Spawner?.Id == 0xffffffff).ToList();
+
+            // Получаем все npcs и последний идентификатор объекта
+            var allNpcs = WorldManager.Instance.GetAllNpcs();
+            var lastObjId = allNpcs.Last().ObjId++;
+
+            // Освобождаем память, присваивая null
+            npcsInWorld = null;
+            allNpcs = null;
+            GC.Collect(); // Вызываем сборку мусора
+
+            // Создаем словарь для быстрого поиска элементов в npcSpawnersToFile
+            var npcSpawnersDict = new Dictionary<(uint UnitId, float X, float Y), JsonNpcSpawns>();
+            var uniqueKeys = new HashSet<(uint, float, float)>();
+
+            foreach (var spawns in npcSpawnersToFile)
+            {
+                // Формируем ключ для проверки
+                var key = (spawns.UnitId,
+                    spawns.Position.X,
+                    spawns.Position.Y);
+
+                // Проверяем уникальность ключа
+                if (!uniqueKeys.Add(key))
+                {
+                    Logger.Warn($"Дубликат объекта с UnitId {spawns.UnitId} " + $"на позиции ({spawns.Position.X}, {spawns.Position.Y}, {spawns.Position.Z})");
+                    continue;
+                }
+
+                // Если ключ уникален - добавляем в словарь
+                npcSpawnersDict.Add(key, spawns);
+            }
+
+            // Удаляем элементы из npcSpawnersToFile, которые соответствуют removeNpcs
+            Parallel.For(0, removeNpcs.Count, i =>
+            {
+                var npc = removeNpcs[i];
+                var key = (npc.TemplateId, npc.Transform.World.Position.X, npc.Transform.World.Position.Y);
+                if (!npcSpawnersDict.TryGetValue(key, out var value))
+                    return;
+
+                lock (npcSpawnersToFile)
+                    npcSpawnersToFile.Remove(value);
+                npcSpawnersDict.Remove(key);
+            });
+
+            // Добавляем элементы из addNpcs в npcSpawnersToFile
+            Parallel.For(0, addNpcs.Count, i =>
+            {
+                var npc = addNpcs[i];
+                var pos = npc.Transform.World;
+                var newNpcSpawn = new JsonNpcSpawns
+                {
+                    Id = lastObjId++,
+                    UnitId = npc.TemplateId,
+                    Title = GetSpawnName(npc.TemplateId), // обновим Title
+                    Position = new JsonPosition
+                    {
+                        X = pos.Position.X,
+                        Y = pos.Position.Y,
+                        Z = pos.Position.Z,
+                        Roll = pos.Rotation.X.RadToDeg(),
+                        Pitch = pos.Rotation.Y.RadToDeg(),
+                        Yaw = pos.Rotation.Z.RadToDeg()
+                    },
+                    Scale = npc.Scale,
+                };
+
+                lock (npcSpawnersToFile)
+                {
+                    npcSpawnersToFile.Add(newNpcSpawn);
+                }
+            });
+
+            var jsonPathOut = Path.Combine(FileManager.AppPath, "Data", "Worlds", currentWorld.Name, $"npc_spawns_all_{DateTime.Now:yyyyMMdd_HHmmss}.json.add");
+
+            // Запись новых данных в файл
+            var json = JsonConvert.SerializeObject(npcSpawnersToFile, Formatting.Indented, new JsonModelsConverter());
+            File.WriteAllText(jsonPathOut, json);
+
+            stopwatch.Stop();
+
+            // Вывод информации о количестве записей
+            var initialCount = npcSpawnersFromFile.Count;
+            var removedCount = removeNpcs.Count;
+            var addedCount = addNpcs.Count;
+            var finalCount = npcSpawnersToFile.Count;
+
+            SendMessage(messageOutput, $"All npcs have been saved! Time taken: {stopwatch.ElapsedMilliseconds} ms\n" +
+                                       $"Initial count: {initialCount}\n" +
+                                       $"Removed count: {removedCount}\n" +
+                                       $"Added count: {addedCount}\n" +
+                                       $"Final count: {finalCount}");
+
+            Logger.Warn($"All npcs have been saved! Time taken: {stopwatch.ElapsedMilliseconds} ms\n" +
+                        $"Initial count: {initialCount}\n" +
+                        $"Removed count: {removedCount}\n" +
+                        $"Added count: {addedCount}\n" +
+                        $"Final count: {finalCount}");
+        }
+        finally
+        {
+            lock (_saveLock)
+            {
+                _isSavingInProgress = false;
+            }
+        }
     }
 
     private void SaveById(ICharacter character, uint npcObjId, IMessageOutput messageOutput)
@@ -188,16 +257,40 @@ public class NpcSaveSubCommand : SubCommandBase
 
     private List<JsonNpcSpawns> LoadNpcsFromFileByWorld(World world)
     {
-        var jsonPathIn = Path.Combine(FileManager.AppPath, "Data", "Worlds", world.Name, "npc_spawns.json");
-        if (!File.Exists(jsonPathIn))
+        var worldDirectory = Path.Combine(FileManager.AppPath, "Data", "Worlds", world.Name);
+        var jsonFiles = Directory.GetFiles(worldDirectory, "npc_spawns*.json");
+
+        if (jsonFiles.Length == 0)
         {
-            throw new GameException($"File {jsonPathIn} doesn't exists.");
+            Logger.Warn($"No npc_spawns*.json files found in directory {worldDirectory}.");
+            return [];
         }
 
-        var contents = FileManager.GetFileContents(jsonPathIn);
-        Logger.Info($"Loading spawns from file {jsonPathIn} ...");
+        var allNpcs = new List<JsonNpcSpawns>();
 
-        return string.IsNullOrWhiteSpace(contents) ? [] : JsonHelper.DeserializeObject<List<JsonNpcSpawns>>(contents);
+        foreach (var jsonFile in jsonFiles)
+        {
+            var contents = FileManager.GetFileContents(jsonFile);
+            if (string.IsNullOrWhiteSpace(contents))
+            {
+                Logger.Warn($"File {jsonFile} is empty or could not be read.");
+                continue;
+            }
+
+            Logger.Info($"Loading spawns from file {jsonFile} ...");
+
+            var npcs = JsonHelper.DeserializeObject<List<JsonNpcSpawns>>(contents);
+            if (npcs != null)
+            {
+                allNpcs.AddRange(npcs);
+            }
+            else
+            {
+                Logger.Warn($"Failed to deserialize npcs from file {jsonFile}.");
+            }
+        }
+
+        return allNpcs;
     }
 
     private static string GetSpawnName(uint id)
